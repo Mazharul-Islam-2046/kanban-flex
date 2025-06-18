d22e59e14fb7c0590482c7ab6965edc8321c317d
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-06-18 20:45:39 +0600

    Created the Mongoose Schema for User and Tasks

---
+++ src/models/Task.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { IUser } from "./User";
+
+
+export enum TaskStatus {
+    UP_NEXT = "UP_NEXT",
+    TODO = "TODO",
+    IN_PROGRESS = "IN_PROGRESS",
+    DONE = "DONE"
+}
+
+
+export interface ITask extends Document {
+    title: string;
+    description: string;
+    status: TaskStatus;
+    assignedTo: IUser[];
+    createdBy: IUser;
+}
+
+
+const TaskSchema: Schema<ITask> = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: [true, "Title is required"],
+    },
+    description: {
+        type: String,
+        required: [true, "Description is required"],
+    },
+    status: {
+        type: String,
+        required: [true, "Status is required"],
+        enum: Object.values(TaskStatus),
+    },
+    assignedTo: {
+        type: [Schema.Types.ObjectId],
+        ref: "User",
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+}, {
+    timestamps: true,
+    toJSON: {
+        virtuals: true,
+    },
+    toObject: {
+        virtuals: true,
+    },
+}); 
+
+
+const Task = (mongoose.models.Task as mongoose.Model<ITask>) || mongoose.model<ITask>("Task", TaskSchema);
+
+export default Task;
---
+++ src/models/User.ts
@@ -0,0 +1,52 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+
+export interface IUser extends Document {
+    userName: string;
+    name: string;
+    email: string;
+    password: string;
+    avatar: string;
+}
+
+
+const UserSchema: Schema<IUser> = new mongoose.Schema({
+    userName: {
+        type: String,
+        required: [true, "User Name is required"],
+    },
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        match: /(?=^.{6,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)[0-9a-zA-Z!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{6,}/,
+        select: false
+    },
+    avatar: {
+        type: String,
+        required: [true, "Avatar is required"],
+    },
+},
+{
+    timestamps: true,
+    toJSON: {
+        virtuals: true
+    },
+    toObject: {
+        virtuals: true
+    }
+});
+
+
+
+const User = (mongoose.models.User as mongoose.Model<IUser>) || mongoose.model<IUser>("User", UserSchema);
+
+export default User;
