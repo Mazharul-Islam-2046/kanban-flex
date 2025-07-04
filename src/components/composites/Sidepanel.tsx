33d37f0a6cbc073ce4f1a5d1ecb403cdf0d6cbff
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-07-04 07:25:14 +0600

    Played a bit with the sidebar options

--- src/components/composites/Sidepanel.tsx
+++ src/components/composites/Sidepanel.tsx
@@ -2,206 +2,206 @@
 
 import * as React from "react"
 import {
-  AudioWaveform,
-  BookOpen,
-  Bot,
-  Command,
-  Frame,
-  GalleryVerticalEnd,
-  LayoutDashboardIcon,
-  Map,
-  PieChart,
-  Settings2,
-  SquareTerminal,
+    AudioWaveform,
+    Command,
+    FolderKanban,
+    Frame,
+    GalleryVerticalEnd,
+    Home,
+    LayoutDashboardIcon,
+    ListTodo,
+    Map,
+    PieChart,
+    Settings2,
 } from "lucide-react"
 import {
     Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarRail,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarRail,
 } from "@/components/ui/sidebar"
 
 // This is sample data.
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  teams: [
-    {
-      name: "Acme Inc",
-      logo: GalleryVerticalEnd,
-      plan: "Enterprise",
+    user: {
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "/avatars/shadcn.jpg",
     },
-    {
-      name: "Acme Corp.",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: Command,
-      plan: "Free",
-    },
-  ],
-  navMain: [
-    {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
-      isActive: true,
-      items: [
+    teams: [
         {
-          title: "History",
-          url: "#",
+            name: "Acme Inc",
+            logo: GalleryVerticalEnd,
+            plan: "Enterprise",
         },
         {
-          title: "Starred",
-          url: "#",
+            name: "Acme Corp.",
+            logo: AudioWaveform,
+            plan: "Startup",
         },
         {
-          title: "Settings",
-          url: "#",
+            name: "Evil Corp.",
+            logo: Command,
+            plan: "Free",
         },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
+    ],
+    navMain: [
         {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
+            title: "Home",
+            url: "#",
+            icon: Home,
+            isActive: true,
+            items: [
+                {
+                    title: "History",
+                    url: "#",
+                },
+                {
+                    title: "Starred",
+                    url: "#",
+                },
+                {
+                    title: "Settings",
+                    url: "#",
+                },
+            ],
         },
         {
-          title: "Get Started",
-          url: "#",
+            title: "Projects",
+            url: "#",
+            icon: FolderKanban,
+            items: [
+                {
+                    title: "Genesis",
+                    url: "#",
+                },
+                {
+                    title: "Explorer",
+                    url: "#",
+                },
+                {
+                    title: "Quantum",
+                    url: "#",
+                },
+            ],
         },
         {
-          title: "Tutorials",
-          url: "#",
+            title: "My Tasks",
+            url: "#",
+            icon: ListTodo,
+            items: [
+                {
+                    title: "Introduction",
+                    url: "#",
+                },
+                {
+                    title: "Get Started",
+                    url: "#",
+                },
+                {
+                    title: "Tutorials",
+                    url: "#",
+                },
+                {
+                    title: "Changelog",
+                    url: "#",
+                },
+            ],
         },
         {
-          title: "Changelog",
-          url: "#",
+            title: "Settings",
+            url: "#",
+            icon: Settings2,
+            items: [
+                {
+                    title: "General",
+                    url: "#",
+                },
+                {
+                    title: "Team",
+                    url: "#",
+                },
+                {
+                    title: "Billing",
+                    url: "#",
+                },
+                {
+                    title: "Limits",
+                    url: "#",
+                },
+            ],
         },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
+    ],
+    projects: [
         {
-          title: "General",
-          url: "#",
+            name: "Design Engineering",
+            url: "#",
+            icon: Frame,
         },
         {
-          title: "Team",
-          url: "#",
+            name: "Sales & Marketing",
+            url: "#",
+            icon: PieChart,
         },
         {
-          title: "Billing",
-          url: "#",
+            name: "Travel",
+            url: "#",
+            icon: Map,
         },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
+    ],
 }
 
 console.log(data);
 
 export default function Sidepanel({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar collapsible="icon" {...props}>
-      <SidebarHeader>
-        <div></div>
-      </SidebarHeader>
-      <SidebarContent>
-        <div>
-            <SidebarMenu>
-                <SidebarMenuButton>
-                    <div className="flex justify-center items-center gap-2 w-full">                        
-                        <LayoutDashboardIcon/>
-                        <p className="text-2xl">KanbanFlex</p>                        
-                    </div>
-                </SidebarMenuButton>
-                <div className="h-0.5 w-4/5 bg-[#ffffff20] mx-auto"></div>
-            </SidebarMenu>
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <div></div>
+            </SidebarHeader>
             <SidebarContent>
-                <SidebarGroup>
+                <div>
                     <SidebarMenu>
-                        {data.navMain.map((item) => (
-                            <SidebarMenuItem
-                                key={item.title}
-                                title={item.title}
-                            >
-                                <SidebarMenuButton>
-                                    <item.icon className="h-4 w-4" />
-                                    <span className="ml-2">{item.title}</span>
-                                </SidebarMenuButton>
-                                
-                            </SidebarMenuItem>
-                        ))}
+                        <SidebarMenuItem className="flex items-center justify-center">
+                            <SidebarMenuButton className="flex items-center">
+                                <LayoutDashboardIcon />
+                                <span className="text-2xl">KanbanFlex</span>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                        <div className="h-0.5 w-4/5 bg-[#ffffff20] mx-auto"></div>
                     </SidebarMenu>
-                </SidebarGroup>
+                    <SidebarContent>
+                        <SidebarGroup>
+                            <SidebarMenu className="gap-3">
+                                {data.navMain.map((item) => (
+                                    <SidebarMenuItem
+                                        key={item.title}
+                                        title={item.title}
+                                    >
+                                        <SidebarMenuButton>
+                                            <item.icon className="" />
+                                            <span className="ml-2 text-xl">{item.title}</span>
+                                        </SidebarMenuButton>
+
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroup>
+                    </SidebarContent>
+
+                    <SidebarFooter>
+
+                    </SidebarFooter>
+                </div>
             </SidebarContent>
-
             <SidebarFooter>
-
             </SidebarFooter>
-        </div>
-      </SidebarContent>
-      <SidebarFooter>
-      </SidebarFooter>
-      <SidebarRail />
-    </Sidebar>
-  )
+            <SidebarRail />
+        </Sidebar>
+    )
 }
