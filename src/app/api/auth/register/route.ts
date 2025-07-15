1d25039cdb849119c156b874842be3e8e5d0af0b
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-07-15 23:33:48 +0600

    fixed the type any issue

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,12 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/User";
-import { connectDB } from "@/db/client"
 import bcrypt from "bcryptjs";
+import dbConnect from "@/db/client";
 
 
 
 export const POST = async (req: NextRequest) => {
-    await connectDB();
+    await dbConnect();
     const { userName, email, password, name } = await req.json();
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ userName, email, password: hashedPassword, name });
