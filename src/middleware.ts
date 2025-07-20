14a4c639b406df4de97e1400122ab632610b78d3
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-07-21 02:59:39 +0600

    fixed the middleware req type to NextReq type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 // middleware.ts
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware(req) {
+export function middleware(req: NextRequest) {
   // Example: check for a session cookie (Edge-compatible)
   const hasSession = req.cookies.get('next-auth.session-token') || req.cookies.get('__Secure-next-auth.session-token');
   const { pathname } = req.nextUrl;
