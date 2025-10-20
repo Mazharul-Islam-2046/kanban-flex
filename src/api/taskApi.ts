4ae6a7e71d3c63901f025cd4078dc7f0cd4d0211
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-10-21 00:55:00 +0600

    added the nutation apis

--- src/api/taskApi.ts
+++ src/api/taskApi.ts
@@ -15,10 +15,30 @@ const taskApi = createApi({
             query: () => `/tasks`,
             providesTags: ["Task"],
         }),
+        getTaskById: builder.query<ITask, string>({
+            query: (id) => `/tasks/${id}`,
+            providesTags: ["Task"],
+        }),
+        createTask: builder.mutation<ITask, Partial<ITask>>({
+            query: (task) => ({
+                url: "/tasks",
+                method: "POST",
+                body: task,
+            }),
+            invalidatesTags: ["Task"],
+        }),
+        updateTask: builder.mutation<ITask, { id: string; updates: Partial<ITask> }>({
+            query: ({ id, updates }) => ({
+                url: `/tasks/${id}`,
+                method: "PATCH",
+                body: updates,
+            }),
+            invalidatesTags: ["Task"],
+        }),
     }),
 });
 
 
 
 
-export default taskApi
+export default taskApi;
